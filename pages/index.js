c2e2d1b59d263d054f4c618397642bfd34f01220
Satoshi <uglysatoshi@ya.ru>
2024-01-01 01:42:44 +0300

    adding some structure and first "words"

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="grey" p={3} mb={6} align="center">
+                Hello, I'm a beginner full-stack developer based in Russia
+            </Box>
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Koltyga Valery
+                    </Heading>
+                    <p> Student of SUITD / Full-stack Developer </p>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
