9eff264e84a350da2763a25d2df08109cb2e468d
Satoshi <uglysatoshi@ya.ru>
2024-01-01 09:17:59 +0300

    fix: (Hydration Error)

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import Logo from './logo'
 import NextLink from 'next/link'
 import {
@@ -15,34 +16,42 @@ import {
     useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
-import ThemeToggleButton from "./theme-toggle-button";
+import ThemeToggleButton from './theme-toggle-button'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
-    const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+    const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
     return (
-        <NextLink href={href}>
-            <Link
-                p={2}
-                bg={active ? 'glassTeal' : undefined}
-                color={active ? '#202023' : inactiveColor}
-            >
-                {children}
-            </Link>
-        </NextLink>
+        <Link
+            as={NextLink}
+            href={href}
+            scroll={false}
+            p={2}
+            bg={active ? 'grassTeal' : undefined}
+            color={active ? '#202023' : inactiveColor}
+            target={target}
+            {...props}
+        >
+            {children}
+        </Link>
     )
 }
 
+const MenuLink = forwardRef((props, ref) => (
+    <Link ref={ref} as={NextLink} {...props} />
+))
+
 const Navbar = props => {
     const { path } = props
+
     return (
         <Box
             position="fixed"
             as="nav"
             w="100%"
             bg={useColorModeValue('#ffffff40', '#20202380')}
-            style={{backdropFilter: 'blur(10px)'}}
-            zIndex={1}
+            css={{ backdropFilter: 'blur(10px)' }}
+            zIndex={2}
             {...props}
         >
             <Container
@@ -54,40 +63,59 @@ const Navbar = props => {
                 justify="space-between"
             >
                 <Flex align="center" mr={5}>
-                    <Heading as="h1" size="lg" letterSpacing={'tighter'} >
-                        <Logo/>
+                    <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+                        <Logo />
                     </Heading>
                 </Flex>
+
                 <Stack
                     direction={{ base: 'column', md: 'row' }}
-                    display={{ base:'none', md: 'flex' }}
+                    display={{ base: 'none', md: 'flex' }}
                     width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
-                    mt={{ base: 4, nmd: 0 }}
+                    mt={{ base: 4, md: 0 }}
                 >
-                    <LinkItem href="#" path={path}>
+                    <LinkItem href="/works" path={path}>
                         Works
                     </LinkItem>
-                    <LinkItem href="#" path={path}>
-                        Posts
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/craftzdog/craftzdog-homepage"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        pl={2}
+                    >
+                        Source
                     </LinkItem>
                 </Stack>
+
                 <Box flex={1} align="right">
-                    <ThemeToggleButton/>
-                    <Box ml={2} display={{base: 'inline-block', md: 'none'}}>
-                        <Menu>
-                            <MenuButton as={IconButton} icon={<HamburgerIcon/>} variat="outline" aria-label="Options"/>
+                    <ThemeToggleButton />
+
+                    <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+                        <Menu isLazy id="navbar-menu">
+                            <MenuButton
+                                as={IconButton}
+                                icon={<HamburgerIcon />}
+                                variant="outline"
+                                aria-label="Options"
+                            />
                             <MenuList>
-                                <NextLink href="/" passHref>
-                                    <MenuItem as={Link}> About </MenuItem>
-                                </NextLink>
-                                <NextLink href="#" passHref>
-                                    <MenuItem as={Link}> Works </MenuItem>
-                                </NextLink>
-                                <NextLink href="https://github.com/uglysatoshi/profile-app" passHref>
-                                    <MenuItem as={Link}> Source</MenuItem>
-                                </NextLink>
+                                <MenuItem as={MenuLink} href="/">
+                                    About
+                                </MenuItem>
+                                <MenuItem as={MenuLink} href="/works">
+                                    Works
+                                </MenuItem>
+                                <MenuItem
+                                    as={Link}
+                                    href="https://github.com/craftzdog/craftzdog-homepage"
+                                >
+                                    Source
+                                </MenuItem>
                             </MenuList>
                         </Menu>
                     </Box>
