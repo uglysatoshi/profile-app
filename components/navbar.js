d0fad6c1dbffb223e11a90fd08b562b3f5dc4e62
Satoshi <uglysatoshi@ya.ru>
2024-01-01 19:36:21 +0300

    adding icons for works and git

--- components/navbar.js
+++ components/navbar.js
@@ -16,6 +16,7 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
+import { IoLogoGithub, IoConstruct } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
@@ -71,18 +72,27 @@ const Navbar = props => {
                     flexGrow={1}
                     mt={{ base: 4, md: 0 }}
                 >
-                    <LinkItem href="/works" path={path}>
-                        Works
-                    </LinkItem>
                     <LinkItem
-                        target="_blank"
-                        href="https://github.com/craftzdog/craftzdog-homepage"
+                        href="/works"
                         path={path}
                         display="inline-flex"
                         alignItems="center"
                         style={{ gap: 4 }}
                         pl={2}
                     >
+                        <IoConstruct/>
+                        Works
+                    </LinkItem>
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/uglysatoshi/profile-app"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        pl={2}
+                    >
+                        <IoLogoGithub/>
                         Source
                     </LinkItem>
                 </Stack>
@@ -107,7 +117,7 @@ const Navbar = props => {
                                 </MenuItem>
                                 <MenuItem
                                     as={Link}
-                                    href="https://github.com/craftzdog/craftzdog-homepage"
+                                    href="https://github.com/uglysatoshi/profile-app"
                                 >
                                     Source
                                 </MenuItem>
