7ccb5ed049b4153b444ff2a25c0f70409858ffff
Satoshi <uglysatoshi@ya.ru>
2024-01-02 09:30:23 +0300

    fix: (toggle positioning on mobile)

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,5 +1,5 @@
-import { AnimatePresence, motion } from "framer-motion"
-import { IconButton, useColorMode, useColorModeValue} from "@chakra-ui/react"
+import { AnimatePresence, motion } from 'framer-motion'
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
 const ThemeToggleButton = () => {
@@ -8,19 +8,19 @@ const ThemeToggleButton = () => {
     return (
         <AnimatePresence mode='wait' initial={false}>
             <motion.div
+                style={{ display: 'inline-block' }}
                 key={useColorModeValue('light', 'dark')}
-                initial={{y: -20, opacity: 0}}
-                animate={{y: 0, opacity: 1}}
-                exit={{y: 20, opacity: 0}}
-                transition={{duration: 0.2}}
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 20, opacity: 0 }}
+                transition={{ duration: 0.2 }}
             >
                 <IconButton
                     aria-label="Toggle theme"
                     colorScheme={useColorModeValue('purple', 'orange')}
-                    icon={useColorModeValue(<MoonIcon/>, <SunIcon/>)}
+                    icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
                     onClick={toggleColorMode}
-                >
-                </IconButton>
+                ></IconButton>
             </motion.div>
         </AnimatePresence>
     )
