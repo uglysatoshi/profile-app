5d778936d9301abb38cbd1373e40241dfb159b26
Satoshi <uglysatoshi@ya.ru>
2024-01-03 04:34:46 +0300

    ??

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // images: { unoptimized: true }
+    reactStrictMode: true,
+    swcMinify: true
 }
 
 module.exports = nextConfig
