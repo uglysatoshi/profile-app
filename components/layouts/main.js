36c1e79fe2ea67e6c21077b306aba4e26f389e47
Satoshi <uglysatoshi@ya.ru>
2024-01-05 10:46:54 +0300

    added icon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,10 +7,11 @@ const Main = ({ children, router }) => {
     return (
         <Box as="main" pb={8}>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/ico.png"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
             </Head>
 
-            <Navbar path={ router.asPath } />
+            <Navbar path={router.asPath}/>
             <Container maxW="container.md" pt={14}>
                 {children}
                 <Footer />
